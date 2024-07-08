8a6ac269563196d460ef6439d07ee437f50122c7
Jonathan Pai <jonathantspai@gmail.com>
2024-07-07 16:14:11 -1000

    Fixed seg fault error within hashmap.h

--- test/test_hashmap.cpp
+++ test/test_hashmap.cpp
@@ -11,12 +11,16 @@ int main() {
     assert(hm.size() == 0);
 
     hm.insert("keyone", 5);
+    
+    hm.insert("keyfour", 299);
 
     assert(*(hm.find("keyone")) == 5);
+ 
+    cout << "Value of keyone: " << *hm.find("keyone") << endl;
 
-    // assert(hm.find("keytwo") == nullptr);
+    assert(hm.find("keytwo") == nullptr);
 
-    // assert(hm.find("keythree") == nullptr);
+    assert(hm.find("keythree") == nullptr);
 
 
     cout << "All test cases passed!" << endl;
