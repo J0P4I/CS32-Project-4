4e1ae223d095553ccbc84e4e7a825b397bdccfc6
Jonathan Pai <jonathantspai@gmail.com>
2024-07-14 17:26:50 -1000

    Added rehashing within HashMap class.

--- include/hashmap.h
+++ include/hashmap.h
@@ -13,9 +13,9 @@ class HashMap
     public:
         HashMap(double max_load = 0.75);
 
-        ~HashMap() = default;
+        ~HashMap();
 
-        inline unsigned long size() const { return m_hashmap_size; }
+        inline unsigned long size() const { return m_items; }
 
         void insert(const std::string& key, const T& value);
 
@@ -23,9 +23,9 @@ class HashMap
 
         const T* find(const std::string& key) const;
 
-        T* find(const std::string& key) { //non const version of find
-            const auto& hm = this;
-            return const_cast<T*>(hm->find(key)); //converts the const T* to T*
+        T* find(const std::string& key) {
+            const auto& hm = *this;
+            return const_cast<T*>(hm.find(key));
         }
     private:
         //Delete copy constructor/assignment operator bc hashmaps have no need for them
@@ -35,26 +35,41 @@ class HashMap
         //Helper functions
         unsigned int hash(std::string key) const;
 
+        void rehash();
+
         //Member variables
-        std::vector<std::list<std::pair<std::string, T>>> m_buckets; //use a pointer for std::pair for effecient use of memory (dynamic allocation)
-        unsigned long m_hashmap_size;
+        std::vector<std::list<std::pair<std::string, T>*>> m_buckets; 
+        unsigned long m_items;
         double m_max_load;
 
 };
 
 template<typename T>
 HashMap<T>::HashMap(double max_load) 
-: m_hashmap_size(0), m_max_load(max_load), m_buckets(10) 
+: m_items(0), m_max_load(max_load), m_buckets(10) 
 {}
 
 template<typename T>
+HashMap<T>::~HashMap() {
+    for (auto& vec_it = m_buckets.begin(); vec_it != m_buckets.end(); vec_it++) {
+        for (auto& list_it = vec_it->begin(); list_it != vec_it->end(); list_it++) {
+            delete *list_it;
+        }
+    }
+}
+
+template<typename T>
 void HashMap<T>::insert(const std::string& key, const T& value) {
-    m_hashmap_size++;
+    m_items++;
+
+    if (static_cast<float>(m_items) / m_buckets.size >= m_max_load) {
+        rehash();
+    }
     
     //If load factor exceeded, rehash buckets
-    size_t hashed_index = hash(key);
-    
-    std::pair<std::string, T> new_pair = std::pair<std::string, T>(key, value);
+    size_t hashed_index = hash(key, m_buckets.size());
+
+    std::pair<std::string, T>* new_pair = new std::pair<std::string, T>(key, value);
 
     m_buckets[hashed_index].push_back(new_pair);
 }
@@ -67,28 +82,41 @@ T& HashMap<T>::operator[](const std::string& key) {
 template<typename T>
 const T* HashMap<T>::find(const std::string& key) const {
     //hash the string key to a index
-    size_t hashed_index = hash(key);
+    size_t hashed_index = hash(key, m_buckets.size());
 
     auto linked_list = m_buckets[hashed_index];
     
-    return m_buckets[hashed_index].front().first;
-    // //iterate through the linked list and search for the correct node
-    // for (auto it = linked_list.begin(); it != linked_list.end(); it++) {
-    //     //if the key's match, return the iterator (a reference to the class)
-    //     if (((*it).first) == key) {
-    //         return it;
-    //     } 
-    // }
-    // return nullptr;
+    for (const auto& it = linked_list.begin(); it != linked_list.end(); it++) {
+        //If the key's match, return the iterator (a reference to the class)
+        if (((*it)->first) == key) { //Dereferencing it provides a pointer to a pair
+            return &(*it)->second;
+        } 
+    }
+    return nullptr;
 }
 
 template<typename T>
-unsigned int HashMap<T>::hash(std::string key) const {
+unsigned int HashMap<T>::hash(std::string key, size_t size) const {
     std::hash<std::string> hash_function;
     size_t hashed_index = hash_function(key);
     //mod the index by the bucket size to ensure a valid index
-    hashed_index %= m_buckets.size();
+    hashed_index %= size;
     return hashed_index;
-} 
+}  
+
+template<typename T>
+void HashMap<T>::rehash() {
+    std::vector<std::list<std::pair<std::string, T>*>> rehashed_buckets(m_buckets.size() * 2);
+
+    for (auto& vector_it : m_buckets) {
+        for (auto& list_it : vector_it) {
+            string key = list_it.first;
+            rehashed_index = hash(key, rehashed_buckets.size());
+            rehashed_buckets[rehashed_index].push_back(list_it);
+        }
+    }
+    
+    m_buckets = rehashed_buckets;
+}
 
 #endif //HASHMAP_H
